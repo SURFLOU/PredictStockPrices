d1a03a4e5e369a8b03207c9d110e8da1abf5d719
bordure <github.recite393@slmails.com>
2025-06-12 16:03:34 +0200

    fix issues with azure function

--- function/function_app.py
+++ function/function_app.py
@@ -1,15 +1,19 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from cashflow.main import main as cashflow_main
+from livestockprices.main import main as livestockprices_main
 import azure.functions as func
-import cashflow.cashflow
-import livestockprices.livestockprices
 
 app = func.FunctionApp()
 
 @app.timer_trigger(schedule="0 */10 11-19 * * 1-5", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def livestockprices(myTimer: func.TimerRequest) -> None:
-    livestockprices.main()
+    livestockprices_main()
 
 @app.timer_trigger(schedule="0 0 19 * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def cashflow(myTimer: func.TimerRequest) -> None:
-    cashflow.main()
+    cashflow_main()
